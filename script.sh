b2cf23c7a23b0a586afeac0ff8895c7f9b0d88f4
Benas137 <111125580+Benas137@users.noreply.github.com>
2024-06-12 12:48:02 +0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+while true
+do
+	random_number=$((1 + RANDOM % 4))
+
+	if [ $random_number = 1 ];
+	then
+		./terminal-parrot & sleep 120; kill $!
+		clear
+
+	elif [ $random_number = 2 ];
+	then
+		imgcat racoon-pedro.gif & sleep 120; kill $!
+		clear
+
+	elif [ $random_number = 3 ];
+	then
+		sl
+		clear
+
+	elif [ $random_number = 4];
+	then
+		fortune
+		sleep 120
+		clear
+
+	fi
+done
